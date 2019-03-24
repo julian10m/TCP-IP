7f8fcc2222f8f6bc0b76d6325a92754d2c167833
julian10m <35936197+julian10m@users.noreply.github.com>
2019-03-24 07:44:26 +0100

    Split Sender_t into multiple independent threads

--- Go_Back_N/Sender_Receiver_Simulation.py
+++ Go_Back_N/Sender_Receiver_Simulation.py
@@ -3,28 +3,32 @@ import threading
 import time
 import random 
 import queue
+from pprint import pprint
 
 MAX_DATA_INTERVAL   = 2
 SN_INIT             = 0
 WINDOW_SIZE         = 9
 MOD_SN_MAX          = WINDOW_SIZE + 1 
-
+RTT_0 = 10
 LENGTH_DATA = 10
 
-data_generated  = threading.Event()
-timer_expired   = threading.Event()
-received_ACK    = threading.Event()
-
-packets_in_the_channel = []
+base = SN_INIT
+nextseqnum = SN_INIT
+packets_in_the_channel = [None for _ in range(MOD_SN_MAX)]
 q_data = queue.Queue()
 q_ACKs = queue.LifoQueue()
 
-# class Window():
-#     def __init__(self, sn_init, sn_max, window_size):
-#         self.window_size = window_size 
-#         self.base = sn_init
-#         self.nextseqnum = sn_init
-#         self.sn_max  = sn_max
+start_timer         = threading.Event()
+update_timer_status = threading.Event()
+stop_timer          = threading.Event()
+updated_window      = threading.Event()
+
+DELTA_T_ACKS    = 1.7
+DELTA_T_DATA    = 0.5
+DELTA_T_PRINT   = 0.5
+
+Q_ACKs_SIM  = 5
+Q_DATA_SIM  = 2*WINDOW_SIZE
 
 class Packet():
     def __init__(self, data, sn, chk):
@@ -32,139 +36,149 @@ class Packet():
         self.sn   = sn
         self.chk  = chk    
 
-def set_timer_event():
-    # print("Timer expired!!!")
-    timer_expired.set()
+def make_seg(d, sn, chk):
+    return Packet(d, sn, chk)    
 
-def generate_data():
-    for i in range(2*WINDOW_SIZE):
-        d = ''.join(random.choices(string.ascii_uppercase + string.digits, k=LENGTH_DATA))
-        time.sleep(0.5)
-        q_data.put(d)
-    q_data.put(None)
-    time.sleep(5)
-    
+def read_ACK(ack):
+    return ack.sn
+
+def retransmit_packets():
+    t_now = time.time()
+    print("\tResending seqnum :", end =" ")
+    for seqnum in range(base, base+WINDOW_SIZE):
+        seqnum = seqnum % MOD_SN_MAX
+        if nextseqnum == seqnum:
+            break
+        packets_in_the_channel[seqnum][1] = t_now
+        print("{}, ".format(seqnum), end=" ")
+    print("")
+
+def set_timer_event():
+    start_timer.set()
+    update_timer_status.set()
+
+def window_controller():
+    while True:
+        d = q_data.get()
+        if d is None:
+            break
+        global nextseqnum
+        while not ((nextseqnum < base+WINDOW_SIZE) and (nextseqnum != (base+WINDOW_SIZE) % MOD_SN_MAX)):
+            print("waiting")
+            updated_window.wait()
+        updated_window.clear()    
+        packets_in_the_channel[nextseqnum] = [make_seg(d, nextseqnum, 0), time.time()]
+        print("Packet sent: base = {} , seqnum = {}".format(base, nextseqnum))
+        # print("Packet sent: base = {} , seqnum = {}".format(base, nextseqnum), end=" ")
+        # print("Packet sent: data={}, base = {} , seqnum = {}".format(d, base, nextseqnum), end=" ")
+        # pprint(packets_in_the_channel)
+        # for ind,packet in enumerate(packets_in_the_channel):
+            # if packet!=None:
+                # print(ind, packet[0], packet[1])
+                # print(ind, packet.sn, packet.data)
+        if base == nextseqnum:
+            print("---> Timer signalling", end=" ")
+            start_timer.set()
+        nextseqnum = (nextseqnum + 1) % MOD_SN_MAX
+        print("nextseqnum = {}, base+m={}".format(nextseqnum, (base+WINDOW_SIZE) % MOD_SN_MAX))
+
+def timer_controller():
+    timer = threading.Timer(RTT_0, set_timer_event)
+    while True:
+        # print("Timer Alive?", timer.is_alive())
+        if not timer.is_alive():
+            print("------> Waiting for Timer signalling")
+            start_timer.wait()
+            print(50*"*", "Starting Timer")
+            timer = threading.Timer(RTT_0, set_timer_event)
+            timer.start()
+            start_timer.clear()
+        while True:
+            print("------> Waiting for Timer Update event")
+            update_timer_status.wait()
+            update_timer_status.clear()
+            if not timer.is_alive():
+                print("------> Timer has expired!!")
+                retransmit_packets()
+                # print(20*"*", "------> Restarting Timer")
+                break
+            if stop_timer.is_set():
+                timer.cancel()
+                stop_timer.clear()
+                print("------> Timer has been stopped!!")
+                # print("Timer Alive?", timer.is_alive())
+                break
+            timer.cancel()
+            global base
+            t_base = packets_in_the_channel[base][1]
+            delta_RTT0 = RTT_0 + t_base - time.time()
+            print("Timer readjusted! ---> delta_RTT0 = {}".format(delta_RTT0))
+            timer = threading.Timer(delta_RTT0, set_timer_event)
+            timer.start()
+            
+def ACKs_controller():
+    while True:
+            ACK = q_ACKs.get()
+            # if random.uniform(0, 1) < 0.95:
+            base_temp = read_ACK(ACK)
+            print("ACK = {}".format(base_temp))
+            global base
+            if base_temp in [expected_base % MOD_SN_MAX for expected_base in range(base+1, nextseqnum+WINDOW_SIZE+1)]:
+                for ind in [ind % MOD_SN_MAX for ind in range(base, base+WINDOW_SIZE)]:
+                    if ind == base_temp:
+                        break
+                    packets_in_the_channel[ind] = None 
+                # pprint(packets_in_the_channel)
+                base = base_temp      
+                print("Base updated! {}".format(base))
+                updated_window.set()
+                if base == nextseqnum:
+                    print("---> Stopping signalling")
+                    stop_timer.set()
+                update_timer_status.set()
+
+
 def generate_ACKs():
-    for i in range(10):
-        time.sleep(1.67)
+    for i in range(Q_ACKs_SIM):
+        time.sleep(DELTA_T_ACKS)
         q_ACKs.put(Packet(None, i+1, None))
 
+def generate_data():
+    for i in range(Q_DATA_SIM):
+        d = ''.join(random.choices(string.ascii_uppercase + string.digits, k=LENGTH_DATA))
+        time.sleep(DELTA_T_DATA)
+        q_data.put(d)
+    q_data.put(None)
+    time.sleep(10)
+
 def print_status():
+    old = "0 0"
     while True:
-        print(q_data.qsize(), q_ACKs.qsize())
-        time.sleep(0.5)
+        to_print = "{} {}".format(q_data.qsize(), q_ACKs.qsize())
+        if old!=to_print:
+            print(to_print)
+        time.sleep(DELTA_T_PRINT)
 
-class Sender_t(threading.Thread):
-    def __init__(self, name, sn_init, mod_sn_max, window_size):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.window_size = window_size 
-        self.base = sn_init
-        self.nextseqnum = sn_init
-        self.mod_sn_max  = mod_sn_max
-        print("sn_max = {}".format(mod_sn_max))
-    def run(self):
-        Sender_t.sender_process(self)
+window_controller_t = threading.Thread(target=window_controller)
+timer_controller_t  = threading.Thread(target=timer_controller)
+ACKs_controller_t   = threading.Thread(target=ACKs_controller)
 
-    def make_seg(self, d, sn, chk):
-        return Packet(d, sn, chk)    
-
-    def read_ACK(self, ack):
-        return ack.sn
-
-    def sender_process(self):
-        while True:
-            # event = (event_sender.wait(3)
-            if not q_data.empty():
-                # if self.nextseqnum in [allowed_sn % self.mod_sn_max for allowed_sn in range(self.base, self.base+self.window_size)]:
-                if ((self.nextseqnum < self.base+self.window_size) and (self.nextseqnum != (self.base+self.window_size) % self.mod_sn_max)):
-                    d = q_data.get()
-                    packets_in_the_channel.append(self.make_seg(d, self.nextseqnum, 0))
-                    print("Packet sent: data={}, base = {} , seqnum = {}".format(d, self.base, self.nextseqnum), end=" ")
-                    if self.base == self.nextseqnum:
-                        print("start_timer()", end=" ")
-                        timer = threading.Timer(4, set_timer_event)
-                        timer.start()
-                    self.nextseqnum = (self.nextseqnum + 1) % self.mod_sn_max
-                    print("nextseqnum = {}, base+m={}".format(self.nextseqnum, (self.base+self.window_size) % self.mod_sn_max ))
-
-            if timer_expired.is_set():
-                timer = threading.Timer(4, set_timer_event)
-                timer_expired.clear()
-                timer.start()
-                for seqnum in range(self.base, self.nextseqnum):
-                    seqnum = seqnum % self.mod_sn_max
-                    if self.nextseqnum == seqnum:
-                        break                    
-                    print("Timer expired!!! Resending seqnum {}".format(seqnum))
-                print(50*"*", "Restarted Timer")
-
-            if not q_ACKs.empty():
-                ACK = q_ACKs.get()
-                if random.uniform(0, 1) < 0.95:
-                    base_temp = self.read_ACK(ACK)
-                    print("ACK = {}".format(base_temp))
-                    if base_temp in [expected_base % self.mod_sn_max for expected_base in range(self.base+1, self.nextseqnum+self.window_size+1)] and base_temp:
-                        self.base = base_temp      
-                        print("Base updated! {}".format(self.base))
-                        if self.base == self.nextseqnum:
-                            print("{}, {}".format(self.base, self.nextseqnum))
-                            timer.cancel()
-
-
-sender_t = Sender_t("TCP sender", SN_INIT, MOD_SN_MAX, WINDOW_SIZE)
 generate_data_t = threading.Thread(target=generate_data)
 generate_ACKs_t = threading.Thread(target=generate_ACKs)
 queues_status_t = threading.Thread(target=print_status)
 
-sender_t.daemon = True
-generate_ACKs.daemon = True
-queues_status_t.daemon = True
+queues_status_t.daemon      = True
+window_controller_t.daemon  = True
+timer_controller_t.daemon   = True
+ACKs_controller_t.daemon    = True
 
-sender_t.start()
 generate_data_t.start()
 generate_ACKs_t.start()
 queues_status_t.start()
+window_controller_t.start()
+timer_controller_t.start()
+ACKs_controller_t.start()
+
+
 generate_data_t.join()
 generate_ACKs_t.join()
-
-
-# def generate_data():
-    # for i in range(10):
-        # if data_generated.is_set():
-            # while(data_generated.is_set()):
-                # time.sleep(1)
-        # time.sleep(random.randrange(0, MAX_DATA_INTERVAL+1))
-        # print("Packet generated: {}".format(i+1))
-        # data_generated.set()
-    # print("ended data")
-
-# def window_controller(window):
-#     while True:
-#         if data_generated.is_set():
-#             if window.nextseqnum < ((window.base+window.window_size) % window.sn_max):
-#                 # sndseg[nextseqnum] = make_seg(seqnum, d, chk)
-#                 # to_CR(sndseg[nextseqnum])
-#                 print("Packet sent: seqnum = {}".format(window.nextseqnum), end=" ")
-#                 if window.base == window.nextseqnum:
-#                     print("start_timer()", end=" ")
-#                     timer = threading.Timer(1, set_timer_event)
-#                     timer.start()
-#                 window.nextseqnum = (window.nextseqnum + 1) % window.sn_max
-#                 print("nextseqnum = {}, base+m={}".format(window.nextseqnum, (window.base+window.window_size) % window.sn_max))
-#             else:
-#                 print("refuse_data()")
-#             data_generated.clear()
-
-# window=Window(0, 5, 4)
-
-# generate_data_t     = threading.Thread(target=generate_data)
-# window_controller_t = threading.Thread(target=window_controller, args=(window,), daemon = True)
-
-
-# generate_data_t.start()
-# window_controller_t.start()
-
-# generate_data_t.join()
-
-
